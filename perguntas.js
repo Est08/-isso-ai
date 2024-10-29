0a9b45480fd89f70fdcf63b7a9a162965d69fa0b
Est08 <140622100+Est08@users.noreply.github.com>
2024-10-29 08:27:21 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Matematica II',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores',
+)
+criaCartao(
+    'Portugues',
+    'O que é uma cronica?',
+    'Na literatura e no jornalismo, uma crônica ou crónica é uma narração curta, produzida essencialmente para ser veiculada na imprensa, seja nas páginas de uma revista, seja nas páginas de um jornal ou mesmo na rádio',
+)
+criaCartao(
+    'Matematica I',
+    'O que é ClT?',
+    'Consolidação das Leis de Trabalho',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Segunda Guerra Mundial?',
+    '3 de setembro de 1939',
+)
